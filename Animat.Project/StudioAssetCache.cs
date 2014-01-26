92d0930be25fc35b83e5c59bd4d29104ba78dace
wyvernzora <wyvernzora@gmail.com>
2014-01-26 04:30:15 -0500

    Started working on new asset management
    Added specification draft for asset cache management.

---
+++ Animat.Project/StudioAssetCache.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using libWyvernzora.Core;
+
+namespace Animat.Project
+{
+    /// <summary>
+    /// Asset Cache Manager
+    /// </summary>
+    public class StudioAssetCache : IDisposable
+    {
+        #region Nester Types
+
+        /// <summary>
+        /// Single cache entry
+        /// </summary>
+        private class CacheEntry
+        {
+            public String Name { get; set; }
+            public VInt Address { get; set; }
+            public VInt Length { get; set; }
+        }
+
+        #endregion
+
+        private Dictionary<String, CacheEntry> entries;     // Map of filenames to entries
+        private SortedList<Int32, Int32> holes;     // Sorted list of holes (unused data sections)
+
+
+
+
+
+        #region IDisposable Members 
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
